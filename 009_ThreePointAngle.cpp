1d6c492755363c0de8e3af7700c6bd0f7c99c2e6
ksasazima <ksasazima@sdkk.co.jp>
2022-09-30 16:11:25 +0900

    0929

---
+++ 009_ThreePointAngle.cpp
@@ -0,0 +1,47 @@
+
+/*
+https://atcoder.jp/contests/typical90/tasks/typical90_i
+009 - Three Point Angle（★6）
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstring>
+#include <complex>
+#include <algorithm>
+#define ll long long
+const ll MOD = 1000000007;
+using namespace std;
+
+
+//3点の角度をそれぞれ求めて、最大の角度を返す
+double getKakudo(int p1_x, int p1_y, int p2_x, int p2_y, int p3_x, int p3_y)
+{
+    double res = 0.0;
+    //内積
+
+
+
+
+    return res;
+}
+
+
+int main()
+{
+    //3点の角度
+
+    //全角度に直線を引いて、直線との水平距離が短い3点を選べばいい？？
+    
+    //単純に計算すると
+    //2000C3
+    //2000*1999*1998/(3*2*1)
+
+    
+
+
+
+    return 0;
+}
